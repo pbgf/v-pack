1b07346d485f8544d4f810b8b55c341015d1bb85
pbgf <pbgf@foxmail.com>
2021-01-03 15:42:23 +0800

    dele yarn.lock

--- src/server/plugins/serverStaticPlugin.ts
+++ src/server/plugins/serverStaticPlugin.ts
@@ -2,7 +2,6 @@ import Koa from 'koa';
 import { ICorePlugin } from '../';
 
 const plugin: ICorePlugin = ({ app, root }) => {
-    console.log(root);
     app.use(require('koa-static')(root));
 };
 
